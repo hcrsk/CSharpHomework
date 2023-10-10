57fc221e4ac57432d7bacef18a64955cb407579d
Владислав Лавриненко <mitelvlad99sea@gmail.com>
2023-10-10 21:55:24 +0300

    Homework_1 done

---
+++ Task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float mathPoints = 64F;
+            float informaticsPoints = 83F;
+            float physicsPoints = 49F;
+
+            float sumPoints = mathPoints + informaticsPoints + physicsPoints;
+            float averagePoints = (sumPoints) / 3;
+
+            Console.ReadKey();
+            Console.WriteLine("Сумма баллов: " + sumPoints.ToString("#.##"));
+            Console.ReadKey();
+            Console.WriteLine("Средний балл: " + averagePoints.ToString("#.##"));
+
+        }
+    }
+}
