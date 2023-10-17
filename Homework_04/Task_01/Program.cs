a93123a2fb6fcc5865f6ec45a760a7193e562617
Владислав Лавриненко <mitelvlad99sea@gmail.com>
2023-10-17 21:21:08 +0300

    #

---
+++ Homework_04/Task_01/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Task_01
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Метод, который в качестве входного параметра принимает строковую переменную, а в качестве возвращаемого значения — массив слов
+        /// </summary>
+        /// <param name="rawString">строковая переменная</param>
+        /// <param name="separator">символ который является разделителем слов в строке</param>
+        /// <returns></returns>
+        static string[] StringToWordsArray(string rawString, char separator)
+        {
+            string[] words = new string[1];
+
+            for (int i = 0; i < rawString.Length; i++)
+            {
+                if (rawString[i] != separator)
+                {
+                    words[0] += rawString[i];
+                }
+                else
+                {
+                    string[] newWords = StringToWordsArray(rawString.Substring(++i), separator);
+                    Array.Resize(ref words, words.Length + newWords.Length);
+                    for (int j = 0; j < newWords.Length; j++)
+                    {
+                        words[words.Length - newWords.Length + j] = newWords[j];
+                    }
+                    break;
+                }
+            }
+            return words;
+        }
+
+        /// <summary>
+        ///  Метод, который выводит каждое слово в отдельной строке
+        /// </summary>
+        /// <param name="words">массив слов</param>
+        static void PrintWordsArray(string[] words)
+        {
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            string sentense = Console.ReadLine();
+            Console.WriteLine("Выведем каждое слово в отдельной строке.");
+            PrintWordsArray(StringToWordsArray(sentense, ' '));
+        }
+    }
+}
---
+++ Homework_04/Task_02/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Task_02
+{
+    internal class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="rawString">строка которую мы будем делить на слова</param>
+        /// <param name="separator">символ который является разделителем слов в строке</param>
+        /// <returns></returns>
+        static string[] StringToWordsArray(string rawString, char separator)
+        {
+            return rawString.Split(separator);
+        }
+        /// <summary>
+        /// Метод меняет слова строки местами (в обратной последовательности)
+        /// </summary>
+        /// <param name="inputPhrase">Строка слова в которой будут инвертированы</param>
+        /// <param name="separator">символ который является разделителем слов в строке</param>
+        /// /// <returns></returns>
+        static string ReverseWords(string inputPhrase, char separator)
+        {
+            string reverseString ="";
+
+            string[] wordsArray = StringToWordsArray(inputPhrase, separator);
+            for (int i = wordsArray.Length-1; i >= 0; i--) {
+                reverseString += wordsArray[i] + ' ';
+            }   
+            return reverseString;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+
+            string sentense = Console.ReadLine();
+
+            Console.WriteLine("Выведем каждое слово в отдельной строке.");
+
+            Console.WriteLine(ReverseWords(sentense,' '));
+        }
+    }
+}
