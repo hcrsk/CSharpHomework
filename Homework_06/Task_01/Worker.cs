1f83bd15b95a678cec26e8946631c2be9ac38590
Владислав Лавриненко <mitelvlad99sea@gmail.com>
2023-10-29 17:03:15 +0300

    #

--- Homework_06/Task_01/Worker.cs
+++ Homework_06/Task_01/Worker.cs
@@ -26,7 +26,6 @@ namespace Task_01
             this(id, DateTime.Now, String.Empty, 0, 0, new DateTime(1900, 1, 1, 0, 0, 0), String.Empty)
         {
         }
-
         public Worker(string[] data) :
             this(Convert.ToInt32(data[0]), Convert.ToDateTime(data[1]), data[2], Convert.ToInt32(data[3]), Convert.ToInt32(data[4]), Convert.ToDateTime(data[5]), data[6])
         {
