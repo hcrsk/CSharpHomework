2a0d345f5b40e1f81baf05fe437b16cb2e78395b
Владислав Лавриненко <mitelvlad99sea@gmail.com>
2023-11-01 00:07:48 +0300

    #

---
+++ Homework_07/Task_01/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace Task_01
+{
+    internal class Program
+    {
+        public static void PrintList(List<int> list)
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            List<int> list = new List<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add(rand.Next(101));
+            }
+            PrintList(list);
+
+            list.Remove(0);
+            PrintList(list);
+
+        }
+    }
+}
